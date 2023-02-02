2af11dc17c13c04d23b61302e81daa4837e78a92
valandil08 <62212805+valandil08@users.noreply.github.com>
2023-02-02 23:46:45 +0000

    Add hash helper

---
+++ CSharpUtils/Cryptography/HashHelper.cs
@@ -0,0 +1,56 @@
+﻿using CSharpUtils.Enum;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CSharpUtils.Cryptography
+{
+    public static class HashHelper
+    {
+        public static string HashString(string text, string salt, HashAlg algorithm)
+        {
+            byte[] textData = Encoding.UTF8.GetBytes(text);
+            byte[] saltData = Encoding.UTF8.GetBytes(salt);
+            byte[] result = HashByteArray(textData, saltData, algorithm);
+
+            return Convert.ToBase64String(result);
+        }
+
+        public static byte[] HashByteArray(byte[] data, byte[] salt, HashAlg algorithm)
+        {
+            byte[] array = data.Concat(salt).ToArray();
+
+            switch (algorithm)
+            {
+                case HashAlg.MD5:
+                    using (MD5 hashAlg = MD5.Create())
+                    {
+                        return hashAlg.ComputeHash(array);
+                    }
+
+                case HashAlg.SHA1:
+                    using (SHA1 hashAlg = new SHA1Managed())
+                    {
+                        return hashAlg.ComputeHash(array);
+                    }
+
+                case HashAlg.SHA256:
+                    using (SHA256 hashAlg = new SHA256Managed())
+                    {
+                        return hashAlg.ComputeHash(array);
+                    }
+
+                case HashAlg.SHA384:
+                    using (SHA384 hashAlg = new SHA384Managed())
+                    {
+                        return hashAlg.ComputeHash(array);
+                    }
+
+                default: // HashAlg.SHA512
+                    using (SHA512 hashAlg = new SHA512Managed())
+                    {
+                        return hashAlg.ComputeHash(array);
+                    }
+            }
+        }
+    }
+}
---
+++ CSharpUtils/Enum/HashAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpUtils.Enum
+{
+    public enum HashAlg
+    {
+        MD5 = 1,
+        SHA1 = 2,
+        SHA256 = 3,
+        SHA384 = 4,
+        SHA512 = 5
+    }
+}
